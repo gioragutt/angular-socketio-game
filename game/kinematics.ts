83dc7fd0347c6dd225736848e15308ded2315f84
Giora Guttsait <giora111@gmail.com>
2017-05-02 22:02:59 +0300

    Changed Kinematic.ts file to be TS compliant

--- game/kinematics.ts
+++ game/kinematics.ts
@@ -1,19 +1,16 @@
-class Kinematics {
+export class Kinematics {
+    x = 0;
+    y = 0;
+
     static clamp(min, max, value) {
         return value < min ? min :
                value > max ? max :
                value;
     }
 
-    constructor(speedX = 10, speedY = 10) {
-        this.x = 0;
-        this.y = 0;
-        this.speedX = speedX;
-        this.speedY = speedY;
-    }
+    constructor(public speedX = 10, public speedY = 10) { }
 
-    updatePositionByInput(input) {
-        // console.log(`Update position called with input ${input}`)
+    updatePositionByInput(input: any) {
         if (input.up) {
             this.y -= this.speedY;
         }
@@ -33,8 +30,6 @@ class Kinematics {
         this.y = y;
     }
 
-
-
     clampPosition({width, height}) {
         this.x = Kinematics.clamp(0, width, this.x);
         this.y = Kinematics.clamp(0, height, this.y);
@@ -46,5 +41,3 @@ class Kinematics {
         this.x += this.speedX;
     }
 }
-
-module.exports = Kinematics;
