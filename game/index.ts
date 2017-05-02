9c1b820d0294fc2b6dcf8832e705fc0ac1c6b889
Giora Guttsait <giora111@gmail.com>
2017-05-03 00:30:26 +0300

    Renamed GameServer file name from server.ts to game-server.ts

--- game/index.ts
+++ game/index.ts
@@ -3,5 +3,5 @@ export * from './entity';
 export * from './input';
 export * from './bullet';
 export * from './kinematics';
-export * from './server';
+export * from './game-server';
 export * from './math-methods';
