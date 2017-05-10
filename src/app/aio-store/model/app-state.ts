8cb31027099b22a80b64205de3fefcea6ad7093b
Giora Guttsait <giora111@gmail.com>
2017-05-10 20:47:17 +0300

    Added interface for saving the update from the server on the client
    * what I need to do eventually is to save common TS files for server and client, thus sharing code and/or data models between the client and the server

--- src/app/aio-store/model/app-state.ts
+++ src/app/aio-store/model/app-state.ts
@@ -1,10 +1,16 @@
-import { Player, Bullet, ChatMessage } from './index';
+import { Player, Bullet, ChatMessage, Position } from './index';
 
 export interface AppState {
     id: string;
     players: Player[];
     bullets: Bullet[];
     chatMessages: ChatMessage[];
+}
+
+export interface ServerUpdate {
+    players: Player[];
+    bullets: Bullet[];
+    mousePosition: Position;
 }
 
 export const INITIAL_STATE: AppState = {
